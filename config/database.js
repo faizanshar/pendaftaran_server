c15e40ae736d4ca99d288c94b9fdaa073b53537e
unknown <you@example.com>
2022-12-18 08:38:56 +0700

    initial commit

--- config/database.js
+++ config/database.js
@@ -1,16 +1,23 @@
 var mysql = require("mysql");
+const Sequelize = require("sequelize");
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "pendaftaran",
-  port: "8111",
-});
+// var con = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "pendaftaran",
+//   port: "8111",
+// });
 
-con.connect(function (err) {
-  if (err) throw err;
-  console.log("berhasil");
+// con.connect(function (err) {
+//   if (err) throw err;
+//   console.log("berhasil");
+// });
+
+const db = new Sequelize(process.env.DB_URI, {
+  define: {
+    timestamps: false,
+  },
 });
 
 module.exports = con;
